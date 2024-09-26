01d62aff0a9b27030933a70660e99b2264f22207
gary <wchang18@qq.com>
2024-09-26 22:01:32 +0800

    array tool

---
+++ array.go
@@ -0,0 +1,10 @@
+package gotool
+
+func InArray[T comparable](list []T, value T) bool {
+	for _, v := range list {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wchang18/gotool
+
+go 1.22.0
